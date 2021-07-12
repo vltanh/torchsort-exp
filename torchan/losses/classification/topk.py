24f0b35dff6a9de1a92056127d47c32b0884f1db
vltanh <narubees@gmail.com>
2021-07-12 05:09:54 +0000

    add argument for k

--- torchan/losses/classification/topk.py
+++ torchan/losses/classification/topk.py
@@ -6,16 +6,17 @@ __all__ = ['TopKClassificationLoss']
 
 
 class TopKClassificationLoss(nn.Module):
-    def __init__(self, nclasses, regularization='l2', regularization_strength=1.0):
+    def __init__(self, nclasses, k=1, regularization='l2', regularization_strength=1.0):
         super().__init__()
         self.nclasses = nclasses
         self.regularization = regularization
         self.regularization_strength = regularization_strength
+        self.k = k
 
     def forward(self, output, target):
         ranks = torchsort.soft_rank(
             output, self.regularization, self.regularization_strength
         )
         ranks_label = ranks.gather(-1, target.view(-1, 1))
-        loss = F.relu(self.nclasses - ranks_label).mean()
+        loss = F.relu(self.nclasses - ranks_label - self.k + 1).mean()
         return loss
